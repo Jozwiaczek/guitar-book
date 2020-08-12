5418f91da8d7ccd85b25bc455eefb84d31eeb5d3
Jakub Jóźwiak <kubencki@gmail.com>
2020-08-12 18:26:54 +0200

    Add lerna, add empty yarn.locks, add pre-commit

---
+++ apps/english/content/index.md
@@ -0,0 +1,6 @@
+---
+title: Home
+description: Explore the Guitar Book platform
+---
+
+Index file test
---
+++ apps/english/gatsby-config.js
@@ -0,0 +1,34 @@
+const themeOptions = require('gatsby-theme-guitar-book/theme-options');
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-guitar-book`,
+      options: {
+        ...themeOptions,
+        root: __dirname,
+        subtitle: 'Guitar book English',
+        description: 'How to use the Guitar Book platform',
+        githubRepo: 'jozwiaczek/guitar-book',
+        spectrumPath: '/',
+        sidebarCategories: {
+          null: ['index', 'ballada-o-cyckach']
+        },
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        cache: true,
+        fix: true,
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false
+        }
+      }
+    }
+  ]
+};
