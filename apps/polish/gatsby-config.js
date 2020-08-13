3811da3c834b0e424eaed41eda08e404cfaa9131
Jakub Jóźwiak <kubencki@gmail.com>
2020-08-13 22:35:43 +0200

    Add content

--- apps/polish/gatsby-config.js
+++ apps/polish/gatsby-config.js
@@ -10,7 +10,50 @@ module.exports = {
         baseDir: 'apps/polish',
         subtitle: 'Polish Songs',
         sidebarCategories: {
-          null: ['index', 'ballada-o-cyckach']
+          null: ['index'],
+          'General': [
+            'general/baska',
+            'general/autobiografia',
+            'general/chodz-pomaluj-moj-swiat',
+            'general/jolka-jolka',
+            'general/zegarmistrz-swiatla',
+            'general/lubie-mowic-z-toba',
+            'general/jaka-jestes',
+            'general/kocham-cie-jak-irlandie',
+            'general/nazywali-go-marynarz',
+            'general/statki-na-niebie',
+          ],
+          'Myslovitz': [
+            'myslovitz/dlugosc-dzwieku-samotnosci',
+            'myslovitz/cisza-i-wiatr',
+          ],
+          'Stare Dobre Małżeństwo': [
+            'stare-dobre-malzenstwo/czarny-blues-o-czwartej-nad-ranem',
+            'stare-dobre-malzenstwo/majka',
+            'stare-dobre-malzenstwo/z-nim-bedziesz-szczesliwsza',
+            'stare-dobre-malzenstwo/bieszczadzkie-anioly',
+          ],
+          'Dżem': [
+            'dzem/wehikul-czasu',
+            'dzem/whisky',
+            'dzem/czerwony-jak-cegla',
+          ],
+          'Perfect': [
+            'perfect/autobiografia',
+            'perfect/nie-placz-ewka',
+            'perfect/kołysanka-dla-nieznajomej',
+          ],
+          'Lady Pank': [
+            'lady-pank/stacja-warszawa',
+            'lady-pank/zawsze-tam-gdzie-ty',
+            'lady-pank/mniej-niz-zero',
+          ],
+          'Fuck it ‍😈': [
+            'fuck-it/ballada-o-cyckach',
+            'fuck-it/piosenka-o-komarze',
+            'fuck-it/chryzantemy',
+            'fuck-it/hera-koka-hasz',
+          ],
         },
       }
     },
