9be8a46c1117a174c56b29a464a60abf9c35b7c5
Mateusz Nestorowicz <nestorowicz1@gmail.com>
2020-11-13 17:57:43 +0100

    cleanup

--- gatsby-theme-guitar-book/src/components/shared/verse.js
+++ gatsby-theme-guitar-book/src/components/shared/verse.js
@@ -9,43 +9,36 @@ export const Verse = ({ text, setAllChords }) => {
   const createMarkup = (text) => ({ __html: text });
   const chordRegEx = /^[A-Ha-h][mb#74]?(add9)?(m7)?(maj7)?(sus)?(sus4)?(is)?$/;
 
-  return (
-    <>
-      {lines.map((line, index) => {
-        if (!line || !line.length) {
-          return line;
-        }
-
-        const wordsCount = line.split(SPACE_CODE).filter((w) => !!w && !w.match(chordRegEx)).length;
-        if (!wordsCount) {
-          line = line.split(SPACE_CODE);
-
+  const renderChords = (chordsLine) => {
+    const chords = chordsLine.split(SPACE_CODE);
+    return (
+      <b>
+        {chords.map((item, lineIndex) => {
+          if (!item) return <React.Fragment key={lineIndex}> </React.Fragment>;
+          setAllChords((prev) => (prev.includes(item) ? prev : [...prev, item]));
           return (
-            <span key={index}>
-              {line.map((item, lineIndex) => {
-                if (item === '') return <span key={lineIndex}> </span>;
-                setAllChords((prev) => (prev.includes(item) ? prev : [...prev, item]));
-                return (
-                  <span key={lineIndex}>
-                    <ChordPreview>{item}</ChordPreview>{' '}
-                  </span>
-                );
-              })}
-              {lines.length > 1 && <br />}
-            </span>
+              <React.Fragment key={lineIndex}>
+                <ChordPreview>{item}</ChordPreview>
+                {' '}
+              </React.Fragment>
           );
-        }
-        return (
+        })}
+      </b>
+    );
+  }
+
+  const renderText = (line) => <span
+        style={{ marginBottom: 0 }}
+        dangerouslySetInnerHTML={createMarkup(line)}
+    />
+
+  return lines.map((line, index) => {
+      const wordsCount = line.split(SPACE_CODE).filter((w) => !!w && !w.match(chordRegEx)).length;
+      return (
           <React.Fragment key={index}>
-            <span
-              style={{ marginBottom: 0 }}
-              key={index}
-              dangerouslySetInnerHTML={createMarkup(line)}
-            />
-            {lines.length > 1 && !(index === lines.length - 1) && <br />}
+            {wordsCount ? renderText(line) : renderChords(line)}
+            <br/>
           </React.Fragment>
-        );
-      })}
-    </>
-  );
+      )
+    });
 };
