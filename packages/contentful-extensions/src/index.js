e011656f988860c8cabfab1b0c5a5ff725dbbb40
jakub.jozwiak <kubencki@gmail.com>
2020-11-21 19:50:59 +0100

    Add new route

--- packages/contentful-extensions/src/index.js
+++ packages/contentful-extensions/src/index.js
@@ -8,22 +8,21 @@ import './index.css';
 import WikiText from './extensions/wiki-text';
 import ChordPreview from './extensions/chords-preview';
 
-const App = ({ sdk }) => {
-  // {/*<p>{'use route <host>/#/<extenstion-name>'}</p>*/}
-
-  return (
-    <HashRouter basename="/">
-      <Switch>
-        <Route exact path="/">
-          <ChordPreview sdk={sdk} />
-        </Route>
-        <Route path="/wiki-text">
-          <WikiText sdk={sdk} />
-        </Route>
-      </Switch>
-    </HashRouter>
-  );
-};
+const App = ({ sdk }) => (
+  <HashRouter basename="/">
+    <Switch>
+      <Route exact path="/">
+        <p>{'use route <host>/#/<extenstion-name>'}</p>
+      </Route>
+      <Route path="/chords-preview">
+        <ChordPreview sdk={sdk} />
+      </Route>
+      <Route path="/wiki-text">
+        <WikiText sdk={sdk} />
+      </Route>
+    </Switch>
+  </HashRouter>
+);
 
 init((sdk) => {
   if (sdk.location.is(locations.LOCATION_DIALOG)) {
