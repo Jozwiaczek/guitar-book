147e15ebd0003abfcb4b07c2f6d6b5c173629dd7
Mateusz Nestorowicz <nestorowicz1@gmail.com>
2020-11-22 20:17:42 +0100

    search hot key options

--- packages/gatsby-theme-guitar-book/src/components/search.js
+++ packages/gatsby-theme-guitar-book/src/components/search.js
@@ -32,6 +32,32 @@ const Hotkey = styled.div(verticalAlign, size(24), {
   pointerEvents: 'none',
 });
 
+const HotkeyIcon = styled.div`
+  border: ${border};
+  border-color: ${colors.text4};
+  border-radius: ${borderRadius}px;
+  width: 24px;
+  height: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HotkeyBar = styled.div`
+  height: 35px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  background-color: ${colors.text3};
+  color: ${colors.text4};
+  align-items: center;
+  padding-left: 1rem;
+  gap: 10px;
+  @media (hover: none) and (pointer: coarse) {
+    display: none;
+  }
+`;
+
 const boxShadowColor = transparentize(0.9, 'black');
 export const boxShadow = `${boxShadowColor} 0 2px 12px`;
 
@@ -48,7 +74,6 @@ const Container = styled.div({
 
 const SuggestionBox = styled.div({
   width: '100%',
-  overflowY: 'auto',
   maxWidth: '100%',
   minWidth: 'auto',
   marginTop: 14,
@@ -59,7 +84,15 @@ const SuggestionBox = styled.div({
   maxHeight: `calc(100vh - ${HEADER_HEIGHT}px - 32px)`,
   padding: 0,
   border,
+  display: 'flex',
+  flexDirection: 'column',
 });
+
+const Suggestions = styled.div`
+  flex-grow: 1;
+  max-height: calc(100vh - ${HEADER_HEIGHT}px - 32px - 50px);
+  overflow-y: scroll;
+`;
 
 const Suggestion = styled.div((props) => ({
   color: 'inherit',
@@ -118,7 +151,7 @@ export default function Search(props) {
   const [mouseOver, setMouseOver] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const resultsRefs = useRef([]);
-  const suggestionBoxRef = useRef();
+  const suggestionsRef = useRef();
   const resultLimit = 10;
   const inputRef = useRef(null);
 
@@ -218,7 +251,8 @@ export default function Search(props) {
 
   // select item up/down
   useKey(
-    (event) => (event.key === 'ArrowUp' || event.key === 'ArrowDown') && resultsShown,
+    (event) =>
+      (event.key === 'ArrowUp' || event.key === 'ArrowDown') && resultsShown && result.length,
     (event) => {
       event.preventDefault();
       event.stopPropagation();
@@ -226,7 +260,7 @@ export default function Search(props) {
         const index =
           (prevState + (event.key === 'ArrowUp' ? (prevState === 0 ? 0 : -1) : 1)) %
           Math.min(resultLimit, result.length);
-        suggestionBoxRef.current.scrollTo({
+        suggestionsRef.current.scrollTo({
           behavior: 'smooth',
           block: 'center',
           top: resultsRefs.current[index].offsetTop,
@@ -278,7 +312,7 @@ export default function Search(props) {
     />
   );
 
-  const resultsShown = (focused && value.trim()) || mouseOver;
+  const resultsShown = (focused && value.trim()) || (mouseOver && value.trim());
   return (
     <>
       <Overlay visible={resultsShown} />
@@ -305,39 +339,105 @@ export default function Search(props) {
         />
         {!focused && !value && <Hotkey>/</Hotkey>}
         {resultsShown && (
-          <SuggestionBox ref={suggestionBoxRef}>
-            {result.length ? (
-              <>
-                {result.map((res, index) => (
-                  <Suggestion
-                    key={index}
-                    isSelected={index === selectedIndex}
-                    ref={(el) => (resultsRefs.current[index] = el)}
-                    onMouseEnter={() => {
-                      setSelectedIndex(index);
-                      setMouseOver(true);
-                    }}
-                    onMouseLeave={() => setMouseOver(false)}
-                    onClick={() => navigateTo(res.slug)}
-                  >
-                    <Title>
-                      <HighlightLabel label={res.title} />
-                    </Title>
-                    <Author>
-                      <HighlightLabel label={res.author} />
-                    </Author>
-                    <Lyrics>
-                      <HighlightLabel
-                        label={res.lyrics.substr(res.filter.lyrics < 0 ? 0 : res.filter.lyrics, 20)}
-                      />
-                    </Lyrics>
-                  </Suggestion>
-                ))}
-              </>
-            ) : (
-              <NoResultsInfo>No results found for query &quot;{value}&quot;</NoResultsInfo>
-            )}
-          </SuggestionBox>
+          <>
+            <SuggestionBox>
+              <div>
+                {result.length ? (
+                  <Suggestions ref={suggestionsRef}>
+                    {result.map((res, index) => (
+                      <Suggestion
+                        key={index}
+                        isSelected={index === selectedIndex}
+                        ref={(el) => (resultsRefs.current[index] = el)}
+                        onMouseEnter={() => {
+                          setMouseOver(true);
+                        }}
+                        onMouseMove={() =>
+                          selectedIndex === index ? undefined : setSelectedIndex(index)
+                        }
+                        onMouseLeave={() => setMouseOver(false)}
+                        onClick={() => navigateTo(res.slug)}
+                      >
+                        <Title>
+                          <HighlightLabel label={res.title} />
+                        </Title>
+                        <Author>
+                          <HighlightLabel label={res.author} />
+                        </Author>
+                        <Lyrics>
+                          <HighlightLabel
+                            label={res.lyrics.substr(
+                              res.filter.lyrics < 0 ? 0 : res.filter.lyrics,
+                              20,
+                            )}
+                          />
+                        </Lyrics>
+                      </Suggestion>
+                    ))}
+                  </Suggestions>
+                ) : (
+                  <NoResultsInfo>No results found for query &quot;{value}&quot;</NoResultsInfo>
+                )}
+                <HotkeyBar>
+                  <HotkeyIcon>
+                    <svg width="15" height="15">
+                      <g
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1.2"
+                      >
+                        <path d="M12 3.53088v3c0 1-1 2-2 2H4M7 11.53088l-3-3 3-3" />
+                      </g>
+                    </svg>
+                  </HotkeyIcon>
+                  <div>to select</div>
+                  <HotkeyIcon>
+                    <svg width="15" height="15">
+                      <g
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1.2"
+                      >
+                        <path d="M7.5 3.5v8M10.5 8.5l-3 3-3-3" />
+                      </g>
+                    </svg>
+                  </HotkeyIcon>
+                  <HotkeyIcon>
+                    <svg width="15" height="15">
+                      <g
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1.2"
+                      >
+                        <path d="M7.5 11.5v-8M10.5 6.5l-3-3-3 3" />
+                      </g>
+                    </svg>
+                  </HotkeyIcon>
+                  <div>to navigate</div>
+                  <HotkeyIcon>
+                    <svg width="15" height="15">
+                      <g
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1.2"
+                      >
+                        <path d="M13.6167 8.936c-.1065.3583-.6883.962-1.4875.962-.7993 0-1.653-.9165-1.653-2.1258v-.5678c0-1.2548.7896-2.1016 1.653-2.1016.8634 0 1.3601.4778 1.4875 1.0724M9 6c-.1352-.4735-.7506-.9219-1.46-.8972-.7092.0246-1.344.57-1.344 1.2166s.4198.8812 1.3445.9805C8.465 7.3992 8.968 7.9337 9 8.5c.032.5663-.454 1.398-1.4595 1.398C6.6593 9.898 6 9 5.963 8.4851m-1.4748.5368c-.2635.5941-.8099.876-1.5443.876s-1.7073-.6248-1.7073-2.204v-.4603c0-1.0416.721-2.131 1.7073-2.131.9864 0 1.6425 1.031 1.5443 2.2492h-2.956" />
+                      </g>
+                    </svg>
+                  </HotkeyIcon>
+                  <div>to close</div>
+                </HotkeyBar>
+              </div>
+            </SuggestionBox>
+          </>
         )}
       </Container>
     </>
